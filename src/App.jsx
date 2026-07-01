2d1258bfd618afb968342fad593974d6cc7dd33c
Omri Kepes <omri@cactusoutdoor.co.nz>
2026-07-02 09:53:55 +1200

    trying to fix loading error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect, useMemo } from "react";
+import "./App.css";
 import { loadAllData, createRecord, updateRecord, uploadImage, deleteProduct,
   loadGarmentSamples, createGarmentSample, createSampleVersion, reviewSampleVersion, uploadFile,
   deleteGarmentSample } from "./airtable";
